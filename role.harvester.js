a478b05949f03b2708d88c3c606dab5a2765a213
imad <imad>
2017-01-17 22:40:18 -0500

    1

--- role.harvester.js
+++ role.harvester.js
@@ -13,9 +13,9 @@ var roleHarvester = {
                 }
             }//else go to the nearest source
             else {
-                var sources = creep.room.find(FIND_SOURCES);
-                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
+                var source = creep.pos.findClosestByRange(FIND_SOURCES);
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources);
                 }
             }
         }
