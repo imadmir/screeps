451d61376a116f532daed36fcf53345da160cbfd
imad <imad>
2017-01-18 20:32:55 -0500

    1

--- role.builder.js
+++ role.builder.js
@@ -15,7 +15,7 @@ var roleBuilder = {
         if (creep.memory.building) {
             //Build construction sites
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
+            if(targets.length && creep.room.controller.ticksToDowngrade > 2000) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
