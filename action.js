8c3d3adc6fecd4b08e4ce56dcfbb5303d3db9f75
imad <imad>
2017-01-21 00:47:03 -0500

    1

---
+++ action.js
@@ -0,0 +1,65 @@
+var action = {
+
+    //get dropped energy, or energy from a container
+    PickUpEnergy: function (creep) {
+        var sourceId = '';
+        //if the creep is moving, keep on moving, no need to find a new source
+        if (creep.memory.movingTo != undefined) {
+            sourceId = creep.memory.movingTo;
+        }
+        else {
+            //if the creep has a source id in memroy
+            if (creep.memory.sourceId != undefined) {
+                sourceId = creep.memory.sourceId;
+                var sourceMain = Game.getObjectById(sourceId);
+
+                var droppedSources = sourceMain.pos.findInRange(FIND_DROPPED_RESOURCES, 2);
+                if (droppedSources.length > 0) {
+                    sourceId = droppedSources[0].id;
+                }
+                else {
+                    var containers = sourceMain.pos.findInRange(FIND_STRUCTURES, 2, {
+                        filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_CONTAINER &&
+                                    structure.store[RESOURCE_ENERGY] > 0);
+                        }
+                    });
+                    if (containers.length > 0) {
+                        sourceId = containers[0].id;
+                    }
+                }
+            }
+            if (sourceId == '') {
+                var sourceNew = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
+                sourceId = sourceNew.id;
+            }
+        }
+
+        if (sourceId != '') {
+            creep.memory.movingTo = sourceId;
+            var source = Game.getObjectById(sourceId);
+            if (source !== null && source.amount != undefined) {
+                creep.moveTo(source);
+                creep.pickup(source, source.amount - 1);
+                return true;
+            }
+            //if the source is a contrainer, transfer energy
+            if (source !== null && source.store != undefined) {
+                if (source.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+                return true;
+            }
+
+            //if there is no source. look for a new one
+            creep.memory.movingTo = undefined;
+            
+        }
+
+        return false;
+        
+    }
+
+};
+
+module.exports = action;
