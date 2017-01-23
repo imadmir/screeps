f9113e1e0a372a77999733771d0abdc6ff644d45
imadmir <imadmir@gmail.com>
2017-01-23 13:31:39 -0500

    Update action.js

--- action.js
+++ action.js
@@ -22,7 +22,7 @@ var action = {
         else {
 			var sourcestructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
 					filter: (structure) => {
-						return (structure.structureType == STRUCTURE_CONTAINER
+						return ((structure.structureType == STRUCTURE_CONTAINER
   						        || structure.structureType == STRUCTURE_CONTAINER )
 								&& structure.store[RESOURCE_ENERGY] > 100);
 					}
